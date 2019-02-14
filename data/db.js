ce88b010a17ff5f4369745ac87c719d0891dbd5b
Mike Landers <landers.mike@gmail.com>
2019-02-14 10:24:17 -0800

    enviornment

--- data/db.js
+++ data/db.js
@@ -1,5 +1,6 @@
 const knex = require('knex');
+const db = process.env.DB_ENV || 'development';
 
-const config = require('../knexfile.js').development;
+const config = require('../knexfile.js')[db];
 
 module.exports = knex(config);
---
+++ notes.md
@@ -0,0 +1,3 @@
+# Common Dev Workflow
+
+Development > Testing/QA > User Acceptance Testing/Staging > Production
